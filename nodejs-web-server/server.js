95257f35012bedcbe99e006e953815b2e9206644
waqqir humaid <waqqirhumaid1@gmail.com>
2025-07-24 21:23:08 +0700

    first committ

--- nodejs-web-server/server.js
+++ nodejs-web-server/server.js
@@ -1,10 +1,28 @@
 const http = require("http");
 
 const requestListener = (request, response) => {
+  
   response.setHeader("Content-Type", "text/html");
-
   response.statusCode = 200;
-  response.end("<h1>Waqqir belajar backend!</h1>");
+
+  const {method} = request;
+
+  if (method == "GET") {
+    response.end("<h1>Method GET</h1>");
+  }
+  if (method == 'PUT') {
+    response.statusCode = 200;
+    response.end("<h1>Method PUT</h1>");
+  }
+  if (method == "POST") {
+    response.end("<h1>Method POST</h1>");
+  }
+  if (method == "DELETE") {
+    response.statusCode = 200;
+    response.end("<h1>Method DELETE</h1>");
+  }
+
+
 };
 
 const server = http.createServer(requestListener);
